875c26cdececda67c50a1b8038430c92dc4fa213
ravichandarkolla <ravichandarkolla@gmail.com>
2025-10-25 17:38:50 +0530

    java 8 asynchronous and forEach

---
+++ src/asynchronous/RunAsyncExample.java
@@ -0,0 +1,30 @@
+package asynchronous;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class RunAsyncExample {
+    public static void main(String[] args) {
+        Runnable runnable1 = () -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Hello from Task 1::" + Thread.currentThread().getName());
+
+        };
+        CompletableFuture<Void> taskCompletableFuture1 = CompletableFuture.runAsync(runnable1);
+        System.out.println("Hello from Main::" + Thread.currentThread().getName());
+//        taskCompletableFuture1.join();
+
+        Runnable runnable2 = () -> {
+            System.out.println("Hello from Task 2::" + Thread.currentThread().getName());
+        };
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        CompletableFuture<Void> taskCompletableFuture2 = CompletableFuture.runAsync(runnable2, executorService);
+//        taskCompletableFuture2.join();
+        executorService.shutdown();
+    }
+}
