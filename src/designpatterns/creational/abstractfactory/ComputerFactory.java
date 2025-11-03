308449719c02d3cfdf84640f0ea24b04f24ad93f
ravichandarkolla <ravichandarkolla@gmail.com>
2025-11-03 14:44:22 +0530

    builder and prototype design patterns

--- src/designpatterns/creational/abstractfactory/ComputerFactory.java
+++ src/designpatterns/creational/abstractfactory/ComputerFactory.java
@@ -1,5 +1,8 @@
 package designpatterns.creational.abstractfactory;
 
+// Abstract Factory design pattern provides approach to code for interface rather than implementation.
+// Abstract Factory pattern is “factory of factories” and can be easily extended to accommodate more products, for example we can add another sub-class Laptop and a factory LaptopFactory.
+// Abstract Factory pattern is robust and avoid conditional logic of Factory pattern.
 public class ComputerFactory {
 
     public static Computer getComputer(ComputerAbstractFactory factory){
---
+++ src/designpatterns/creational/builder/Computer.java
@@ -0,0 +1,73 @@
+package designpatterns.creational.builder;
+
+// The builder pattern was introduced to solve some of the problems with factory and abstract Factory design patterns
+// when the object contains a lot of attributes. This pattern solves the issue with a large number of optional parameters
+// and inconsistent state by providing a way to build the object step-by-step and provide a method that will actually
+// return the final Object.
+public class Computer {
+
+    //required parameters
+    private String HDD;
+    private String RAM;
+
+    //optional parameters
+    private boolean isGraphicsCardEnabled;
+    private boolean isBluetoothEnabled;
+
+
+    public String getHDD() {
+        return HDD;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
+    }
+
+    public boolean isBluetoothEnabled() {
+        return isBluetoothEnabled;
+    }
+
+    private Computer(ComputerBuilder builder) {
+        this.HDD = builder.HDD;
+        this.RAM = builder.RAM;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    }
+
+    //Builder Class
+    public static class ComputerBuilder {
+
+        // required parameters
+        private String HDD;
+        private String RAM;
+
+        // optional parameters
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public ComputerBuilder(String hdd, String ram) {
+            this.HDD = hdd;
+            this.RAM = ram;
+        }
+
+        public ComputerBuilder setGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
+            this.isGraphicsCardEnabled = isGraphicsCardEnabled;
+            return this;
+        }
+
+        public ComputerBuilder setBluetoothEnabled(boolean isBluetoothEnabled) {
+            this.isBluetoothEnabled = isBluetoothEnabled;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+
+    }
+
+}
